a2e1bcea8b490ef5cbe042fdf6c40c4760061b8e
Denis Evladov <denis.evladov@domru.ru>
2019-11-12 11:04:08 +0500

    solution

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     static {
@@ -6,17 +9,30 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    CountDownLatch carCdl;
+    Semaphore smp;
+    public static boolean winnerFinishedRace = false;
+
+    public synchronized  static void checkIfIsTheWinner(String name){
+        if(!winnerFinishedRace) {
+            winnerFinishedRace = true;
+            System.out.println("!!!!!В гонке победил " + name + "!!!!!");
+        }
+    }
+
     public String getName() {
         return name;
     }
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CountDownLatch carCdl, Semaphore smp ) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.carCdl = carCdl;
+        this.smp = smp;
     }
 
     @Override
@@ -25,11 +41,32 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            carCdl.countDown();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
+        try {
+            carCdl.await();   // пока счетчик не приравняется нулю, будем стоять на этой строке
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            if(race.getStages().get(i).getClass().getName().equals("Tunnel")){
+                try {
+                    System.out.println(name + " перед туннелем");
+                    smp.acquire();
+                    System.out.println(name + " заезжает в туннель");
+                    race.getStages().get(i).go(this);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println(name + " покинул туннель");
+                    smp.release();
+                }
+            } else race.getStages().get(i).go(this);
+            //System.out.println(race.getStages().get(i).getClass());
+        }
+        System.out.println(name + " завершил гонку");
+        checkIfIsTheWinner(name);
     }
 }
